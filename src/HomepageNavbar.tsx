7f0dea360c5239a23c98e01636245053d3caecc2
zaidmohd16 <zaidmohd16@gmail.com>
2025-03-02 18:44:47 -0700

    `Update HomepageNavbar.tsx to use SVGLogo.svg and add active link styling`

--- src/HomepageNavbar.tsx
+++ src/HomepageNavbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
-import timeTravelIcon from '../src/images/time-clockk.svg';
+import timeTravelIcon from '../src/images/SVGLogo.svg';
 
 export const HomepageNavbar: React.FC = () => {
     return (
@@ -9,21 +9,33 @@ export const HomepageNavbar: React.FC = () => {
             <Container>
             <Nav className="w-100 px-3">
                 <NavLink to="/" className="navbar-brand d-flex align-items-center me-auto">
-                    <img src={timeTravelIcon} alt="Time Use Icon" style={{ width: '34px' }} />
+                    <img src={timeTravelIcon} alt="Time Use Icon" style={{ width: '80px' }} />
                     <h4 className="fw-bold mb-0 ml-2">The Mobility Dashboard</h4>
                 </NavLink>
                 <NavbarBs.Toggle aria-controls="responsive-navbar-nav" />
                 <NavbarBs.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto">
-                        <NavLink to="/" className="nav-link">
-                            Home
-                        </NavLink>
-                        <NavLink to="/about" className="nav-link">
-                            About
-                        </NavLink>
-                        <NavLink to="/travelpurpose" className="nav-link">
-                            Dashboard
-                        </NavLink>
+                    <NavLink 
+                        to="/" 
+                        style={({ isActive }) => isActive ? { fontWeight: 'bold' } : {}}
+                        className="nav-link"
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink 
+                        to="/about" 
+                        style={({ isActive }) => isActive ? { fontWeight: 'bold' } : {}}
+                        className="nav-link"
+                    >
+                        About
+                    </NavLink>
+                    <NavLink 
+                        to="/travelpurpose" 
+                        style={({ isActive }) => isActive ? { fontWeight: 'bold' } : {}}
+                        className="nav-link"
+                    >
+                        Dashboard
+                    </NavLink>
                     </Nav>
                 </NavbarBs.Collapse>
             </Nav>
